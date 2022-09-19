f45968f7d5fbfe811733aba7f2be9832a580304a
Giovani-devp <giovanips.dev@gmail.com>
2022-09-19 19:26:09 -0300

    ajuste enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrlv1: 'http://localhost:5003/api/v1/',
-  imagensUrl: 'http://localhost:5001/'
+  apiUrlv1: 'https://backend-product-v3.herokuapp.com',
+  imagensUrl: 'https://backend-product-v3.herokuapp.com/'
 };
